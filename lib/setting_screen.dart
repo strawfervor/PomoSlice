fcdbd9e843837466e5b3f9fc7f0c2c7e86f8c44a
sf <strawfervor@gmail.com>
2025-10-19 19:02:41 +0100

    added controller for settings

--- lib/setting_screen.dart
+++ lib/setting_screen.dart
@@ -20,11 +20,26 @@ class _SettingScreenState extends State<SettingScreen> {
   late int _breakTime;
   late int _longBreakTime;
 
+  late TextEditingController _pomodoroController;
+  late TextEditingController _breakController;
+  late TextEditingController _longBreakController;
+
   @override
   void initState() {
     super.initState();
     _loadSettings();
     myStateChanger = widget.stateChanger;
+    _pomodoroController = TextEditingController();
+    _breakController = TextEditingController();
+    _longBreakController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _pomodoroController.dispose();
+    _breakController.dispose();
+    _longBreakController.dispose();
+    super.dispose();
   }
 
   Future<void> _loadSettings() async {
@@ -32,6 +47,11 @@ class _SettingScreenState extends State<SettingScreen> {
     _pomodorTime = prefs.getInt('pomodoroTime') ?? 25;
     _breakTime = prefs.getInt('breakTime') ?? 5;
     _longBreakTime = prefs.getInt('longBreakTime') ?? 15;
+
+    //setting values in controllers
+    _pomodoroController.text = _pomodorTime.toString();
+    _breakController.text = _breakTime.toString();
+    _longBreakController.text = _longBreakTime.toString();
   }
 
   void _updateStateChanger() {
@@ -46,7 +66,9 @@ class _SettingScreenState extends State<SettingScreen> {
   Future<void> _saveSettings() async {
     final prefs = await SharedPreferences.getInstance();
     _updateStateChanger();
-    debugPrint("current data: p: $_pomodorTime, b: $_breakTime, lb: $_longBreakTime");
+    debugPrint(
+      "current data: p: $_pomodorTime, b: $_breakTime, lb: $_longBreakTime",
+    );
     prefs.setInt('pomodoroTime', _pomodorTime);
     prefs.setInt('breakTime', _breakTime);
     prefs.setInt('longBreakTime', _longBreakTime);
@@ -55,22 +77,27 @@ class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(context) {
     return SingleChildScrollView(
-      child: Padding(padding: EdgeInsets.symmetric(horizontal: 70), 
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 70),
         child: Column(
           children: [
-            SizedBox(height: 50,),
+            SizedBox(height: 50),
             Text("Settings", style: TextStyle(fontSize: 52)),
-            SizedBox(height: 50,),
+            SizedBox(height: 50),
             TextField(
+              controller: _pomodoroController,
               keyboardType: TextInputType.number,
               inputFormatters: [
                 FilteringTextInputFormatter.allow((RegExp(r'[0-9]'))),
               ],
-              decoration: InputDecoration(label: Text("Pomodoro time (minutes):")),
+              decoration: InputDecoration(
+                label: Text("Pomodoro time (minutes):"),
+              ),
               onChanged: (value) => _pomodorTime = int.parse(value),
             ),
-            SizedBox(height: 20,),
+            SizedBox(height: 20),
             TextField(
+              controller: _breakController,
               keyboardType: TextInputType.number,
               inputFormatters: [
                 FilteringTextInputFormatter.allow((RegExp(r'[0-9]'))),
@@ -78,17 +105,20 @@ class _SettingScreenState extends State<SettingScreen> {
               decoration: InputDecoration(label: Text("Break time (minutes):")),
               onChanged: (value) => _breakTime = int.parse(value),
             ),
-            SizedBox(height: 20,),
+            SizedBox(height: 20),
             TextField(
+              controller: _longBreakController,
               keyboardType: TextInputType.number,
               inputFormatters: [
                 FilteringTextInputFormatter.allow((RegExp(r'[0-9]'))),
               ],
-              decoration: InputDecoration(label: Text("Long break time (minutes):")),
+              decoration: InputDecoration(
+                label: Text("Long break time (minutes):"),
+              ),
               onChanged: (value) => _longBreakTime = int.parse(value),
             ),
-            SizedBox(height: 50,),
-            SquareButton(_saveSettings, buttonText: "Save and Apply")
+            SizedBox(height: 50),
+            SquareButton(_saveSettings, buttonText: "Save and Apply"),
           ],
         ),
       ),
